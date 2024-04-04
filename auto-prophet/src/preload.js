b38b8f31cb2d71683fb786d8abae837ba7db4e5c
Divy Patel <divypatel125710@gmail.com>
2024-04-03 23:20:01 -0400

    update Preload.js

--- auto-prophet/src/preload.js
+++ auto-prophet/src/preload.js
@@ -2,9 +2,15 @@ const path = require('path');
 const { contextBridge } = require('electron')
 const { promises: fsPromises } = require('fs');
 const fs = require('fs');
+const yfinance = require('yfinance');
   
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: require('electron').ipcRenderer,
+});
+
+
+contextBridge.exposeInMainWorld('yfinance',{
+    yfinance: require('yfinance'),
 });
 
 contextBridge.exposeInMainWorld('fs', {
